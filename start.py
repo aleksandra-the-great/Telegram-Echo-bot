747d1f41d62abe5c6f40e990e74d1a6d6b2b2fbe
Alesia Zhauneryk <alesia_zhauneryk@epam.com>
2024-11-08 17:13:42 +0300

    Fixed start,py

--- start.py
+++ start.py
@@ -1,17 +1,30 @@
 from flask import Flask, request, jsonify
 import telebot
-
-
+import os
 
 app = Flask(__name__)
+
+# Получаем токен из переменной окружения
+TOKEN = os.getenv("TOKEN")
+
+# Проверка на случай, если токен отсутствует
+if TOKEN is None:
+    raise ValueError("TOKEN is not set. Please set the TELEGRAM_BOT_TOKEN environment variable.")
+
 bot = telebot.TeleBot(TOKEN)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.get_json()
     message = data.get("message", "")
-    # Отправляем сообщение обратно пользователю
-    bot.send_message(data["user_id"], message)
+    user_id = data.get("user_id", None)
+    
+    if user_id:
+        # Отправляем сообщение обратно пользователю
+        bot.send_message(user_id, message)
+    else:
+        return jsonify({"status": "error", "message": "user_id is missing"}), 400
+    
     return jsonify({"status": "ok"})
 
 if __name__ == "__main__":
